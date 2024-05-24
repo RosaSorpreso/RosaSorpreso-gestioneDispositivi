d8831be5f441ff17cfda40988b67a4060eb97020
RosaSorpreso <rosasorpreso@gmail.com>
2024-05-24 15:17:52 +0200

    creazione repo

---
+++ src/main/java/com/devicesManagment/demo/DevicesManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.devicesManagment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicesManagmentApplication.class, args);
+	}
+
+}
